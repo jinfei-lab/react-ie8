2aeae1ef3baf33aa424a0ecffe8cd5cd1db2bceb
JF <fe.jin@outlook.com>
2019-10-07 23:53:28 +0800

    the first commit project

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './redux/store';
+import {HashRouter as Router} from 'react-router-dom';
+import App from 'components/App/App';
+import 'antd/dist/antd.css';
+
+
+ReactDom.render(
+    <Provider store={store}>
+        <Router>
+            <App/>
+        </Router>
+    </Provider>,
+    document.getElementById('app')
+);
+
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+// import { DatePicker } from 'antd';
+
+
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+
+    _handleClick() {
+        this.setState({
+            count: ++this.state.count
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                this is home~<br />
+                当前计数：{this.state.count}<br />
+                {/* <Button type="primary" onClick={() => this._handleClick()}>自增</Button> */}
+                {/* <DatePicker /> */}
+            </div>
+        )
+    }
+}
